83479ae4e913ada6af76fa5f46bf9176de7bd7e7
Augusto Gonzalez Bonorino <augusto.gonzalez-bonorino@cgu.edu>
2025-05-08 11:55:28 -0700

    added a function to __init__.py

--- llm_survey_steering/data_processing/__init__.py
+++ llm_survey_steering/data_processing/__init__.py
@@ -1,8 +1,8 @@
-
 # llm_survey_steering/data_processing/__init__.py
 from .wvs_processor import (
     load_wvs_data,
     preprocess_wvs_data,
-    generate_prompt_data_from_wvs_split,
-    map_age_to_group
+    generate_prompt_data_from_wvs_split, # Ensure this line is present
+    map_age_to_group,
+    generate_synthetic_wvs_data 
 )
